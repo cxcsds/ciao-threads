189474dc8b70bdd9faeae3828f33b241be8a0925
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2017-02-07 09:42:40 -0500

    remove self from files that get generated

---
+++ build_main_from_notebook.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+
+
+
+notebook_dir=/data/sdsreg/thread_output/Notebooks/
+dest_dir=/data/sdsreg/current_regtest/.tests/threads/
+grp=ciaotest
+
+
+copy_bash_notebook()
+{
+    
+  nb=$1
+  
+  to=`basename $nb | sed 's,\.sh$,.MAIN,g'`
+  
+  cat $nb | \
+    egrep -v '^source /export' | \
+    sed 's,^display,#display,g' | \
+    sed 's,^##REGRESSION_TEST: ,,g' | \
+    cat > $dest_dir/$to
+  chgrp $grp $dest_dir/$to
+  chmod 775 $dest_dir/$to
+
+  echo $to
+  
+}
+
+
+for nn in `/bin/ls ${notebook_dir}/*sh | grep -v build_main_from `
+do
+  copy_bash_notebook $nn
+done
+
